2502f8f25ad6bae99ec4c2d2f22d4d4ce7ae5484
Ikenna <ikenna.porter@gmail.com>
2023-01-28 13:45:33 -0300

    Finish front-end data parser

--- fastapi-server/routers.py
+++ fastapi-server/routers.py
@@ -2,8 +2,19 @@ from fastapi import FastAPI, Depends
 from typing import List
 from queries import TimesheetIn, TimesheetOut, TimesheetRepo
 from db_setup import run_initial_setup
+from fastapi.middleware.cors import CORSMiddleware
 
 router = FastAPI()
+
+origins = ["*"]
+
+router.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @router.get("/api/timesheets", response_model=List[TimesheetOut])
 def get_all_timesheets(repo: TimesheetRepo = Depends()):
